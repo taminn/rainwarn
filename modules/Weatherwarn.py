fddd341a867710c1d2d05cda762b742eaf36b0ac
tangming <37473907+taminn@users.noreply.github.com>
2018-10-29 08:01:07 +0800

    update info

--- modules/Weatherwarn.py
+++ modules/Weatherwarn.py
@@ -16,18 +16,18 @@ def Warn():
     if weather_info == None:
         return []
     if weather_info.today_rain and weather_info.tomorrow_rain:
-        msg.append("今明两天的天气是:{weather}  出门记得带好雨伞".format(
+        msg.append("今明两天的天气是{weather}  出门留心天气".format(
             weather=weather_info.tomorrow_wearther))
     elif weather_info.today_rain:
-        msg.append("今天的天气是{weather}  记得带好雨伞".format(
+        msg.append("今天的天气是{weather}  出门留心天气".format(
             weather=weather_info.today_weather))
     elif weather_info.tomorrow_rain:
-        msg.append("明天的天气是:{weather}  记得准备好雨伞".format(
+        msg.append("明天的天气是{weather}  出门留心天气".format(
             weather=weather_info.tomorrow_wearther))
     if int(weather_info.today_max_tmp)-int(weather_info.tomorrow_max_tmp) >= 10:
-        msg.append("明天的最高气温是:{tmp}℃\n寒潮来袭  记得增添衣物\n".format(
+        msg.append("明天的最高气温是{tmp}℃\n  降温较强记得增添衣物\n".format(
             tmp=weather_info.tomorrow_max_tmp))
-    elif int(weather_info.tomorrow_max_tmp) >= 35:
+    elif int(weather_info.tomorrow_max_tmp) >= 37:
         msg.append("明天的最高气温是:{tmp}℃\n气温过高  注意防暑\n".format(
             tmp=weather_info.tomorrow_max_tmp))
     if len(msg) > 0:
